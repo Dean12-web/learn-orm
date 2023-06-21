ac088bb6f6dbea267703c4619e59a6242e2921d8
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-06-21 22:00:17 +0700

    Pagination Done

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,18 @@ const models = require('../models')
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
   try {
-    const users = await models.User.findAll({
+    const page = parseInt(req.query.page) || 1;
+    const limit = 3;
+    const offset = (page - 1 ) * limit;
+    const {count, rows: users} = await models.User.findAndCountAll({
       include: [
         { model: models.Todo }
-      ]
+      ],
+      order:[['id','asc']],
+      limit,
+      offset
     })
-    res.json(users)
+    res.json({count,users,page})
   } catch (error) {
     res.json({ error })
   }
